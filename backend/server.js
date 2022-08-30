24d1926d88d907dd63129ec14d199dacc4542fa6
whitlocktech <whitlocktech@gmail.com>
2022-08-29 22:34:52 -0500

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,10 @@ const startServer = async () =>{
     })
 
     app.post('/api/links', async (req, res) => {
-        const { id, name, address, adminAddress, category } = req.body
+        const { name, address, adminAddress, category } = req.body
 
         await db.collection('links').insertOne({
+
             name,
             address,
             adminAddress,
@@ -46,6 +47,7 @@ const startServer = async () =>{
         const { name, address, adminAddress, category, note } = req.body
 
         await db.collection('bookmarks').insertOne({
+            id: uuid(),
             name,
             address,
             adminAddress,
@@ -54,6 +56,46 @@ const startServer = async () =>{
         })
         const bookmarks = await db.collection('bookmarks').find({}).toArray()
         res.json(bookmarks)
+    })
+
+    app.put('/api/links/:name', async (req, res) => {
+        const { name: linkName } = req.params
+        const { name, address, adminAddress, category } = req.body
+
+        const link = await db.collection('links').findOne({ name: linkName })
+       
+        link.name = name || link.name
+        link.address = address || link.address
+        link.adminAddress = adminAddress || link.adminAddress
+        link.category = category || link.category
+
+        await db.collection('links').updateOne({ name: linkName }, {
+            $set: link,
+        })
+
+        const updatedLink = await db.collection('links').find({}).toArray()
+
+        res.json(updatedLink)
+    })
+    app.put('/api/bookmarks/:id', async (req, res) => {
+        const { id: bookMarkId } = req.params
+        const { name, address, adminAddress, category, note } = req.body
+
+        const bookmark = await db.collection('bookmarks').findOne({ name: bookMarkId })
+
+        bookmark.name = name || bookmark.name
+        bookmark.address = address || bookmark.address
+        bookmark.adminAddress = adminAddress || bookmark.adminAddress
+        bookmark.category = category || bookmark.category
+        bookmark.note = note || bookmark.note 
+
+        await db.collection('bookmarks').updateOne({ name: bookMarkId }, {
+            $set: bookmark,
+        })
+
+        const updatedBookmark = await db.collection('bookmarks').find({}).toArray()
+
+        res.json(updatedBookmark)
     })
 
     // need to remove hardcoded port
