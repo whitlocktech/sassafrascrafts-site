ce27343f1301f39a20614192be6f2618a72ab87b
whitlocktech <whitlocktech@gmail.com>
2022-08-29 20:22:41 -0500

    add some api endpoints

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 const startServer = async () =>{
     const app = express()
     app.use(cors())
-    app.use(express.json)
+    app.use(express.json())
 
     //need to remove hard coded address and add whatever security mongo has to secure the connection
     const client = await MongoClient.connect('mongodb://localhost:27017', {
@@ -22,7 +22,39 @@ const startServer = async () =>{
         res.json(links) 
     })
 
+    app.get('/api/bookmarks', async (req, res) => {
+        const bookmarks = await db.collection('bookmarks').find({}).toArray()
+        res.json(bookmarks)
+    })
 
+    app.post('/api/links', async (req, res) => {
+        const { id, name, address, adminAddress, category } = req.body
+
+        await db.collection('links').insertOne({
+            name,
+            address,
+            adminAddress,
+            category,
+        });
+
+        const links = await db.collection('links').find({}).toArray()
+
+        res.json(links)
+    });
+
+    app.post('/api/bookmarks', async (req, res) => {
+        const { name, address, adminAddress, category, note } = req.body
+
+        await db.collection('bookmarks').insertOne({
+            name,
+            address,
+            adminAddress,
+            category,
+            note,
+        })
+        const bookmarks = await db.collection('bookmarks').find({}).toArray()
+        res.json(bookmarks)
+    })
 
     // need to remove hardcoded port
     app.listen(8000, () => console.log('Server is Listening on port 3000'))
