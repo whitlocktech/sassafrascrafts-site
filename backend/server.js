97e1d56a156832bdd6cbb72cbb768171861175b7
whitlocktech <whitlocktech@gmail.com>
2022-08-31 22:56:18 -0500

    first blog route works

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 // routes
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
-require("./routes/blog.routes")
+require("./routes/blog.routes")(app)
 
 // set port, listen for requests
 const PORT = process.env.NODE_DOCKER_PORT || 8000;
